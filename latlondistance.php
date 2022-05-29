33cbb989747d9ee952ae8705f13d43f26c61f1fb
jordangantier <47302177+jordangantier@users.noreply.github.com>
2022-05-29 14:48:14 -0400

    Update latlondistance.php

--- latlondistance.php
+++ latlondistance.php
@@ -2,12 +2,12 @@
 
 function latLonDist($lat1, $lon1, $lat2, $lon2)
 {
-    $R = 6371; // Radio de la tierra en km.
+    $radius = 6371; // Radio de la tierra en km.
     $dLat = deg2rad($lat2 - $lat1); // Convierte de grados a radianes.
     $dLon = deg2rad($lon2 - $lon1); // Convierte de grados a radianes.
     $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon / 2) * sin($dLon / 2);
     $b = 2 * atan2(sqrt($a), sqrt(1 - $a));
-    $c = round($R * $b, 2); // Distancia en km con dos decimales de precisión.
+    $c = round($radius * $b, 2); // Distancia en km con dos decimales de precisión.
     return $c;
 };
 
