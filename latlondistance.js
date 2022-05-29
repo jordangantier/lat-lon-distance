a402e3b625ca01ce2ed29185bbcf25e6f16f07ba
jordangantier <47302177+jordangantier@users.noreply.github.com>
2022-05-29 17:11:52 -0400

    Add files via upload

--- latlondistance.js
+++ latlondistance.js
@@ -1,15 +1,14 @@
-function getLatLonDist(lat1, lon1, lat2, lon2) {
-    var radius = 6371; // Radio de la tierra en km.
-    var dLat = deg2rad(lat2 - lat1); // Convierte de grados a radianes.
-    var dLon = deg2rad(lon2 - lon1); // Convierte de grados a radianes.
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+function latlondist(lat1, lon1, lat2, lon2) {
+    var dlat = deg2rad(lat2 - lat1); // Convierte de grados a radianes.
+    var dlon = deg2rad(lon2 - lon1); // Convierte de grados a radianes.
+    var a = Math.sin(dlat / 2) * Math.sin(dlat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dlon / 2) * Math.sin(dlon / 2);
     var b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var c = (radius * b).toFixed(2); // Distancia en km con dos decimales de precisión.
+    var c = (6371 * b).toFixed(2); // Distancia en km con dos decimales de precisión.
     return c;
 }
 
 function deg2rad(deg) {
-    return deg * (Math.PI / 180);
+    return deg * 0.017453292519943295; // deg * Math.PI / 180
 }
 
-console.log(getLatLonDist(-17.645820, -63.336103, -17.644973, -63.141274));
+console.log(latlondist(-17.645820, -63.336103, -17.644973, -63.141274));
--- latlondistance.php
+++ latlondistance.php
@@ -1,18 +1,13 @@
 <?php
 
-function latLonDist($lat1, $lon1, $lat2, $lon2)
+function latlondist($lat1, $lon1, $lat2, $lon2)
 {
-    $radius = 6371; // Radio de la tierra en km.
-    $dLat = deg2rad($lat2 - $lat1); // Convierte de grados a radianes.
-    $dLon = deg2rad($lon2 - $lon1); // Convierte de grados a radianes.
-    $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon / 2) * sin($dLon / 2);
+    $dlat = deg2rad($lat2 - $lat1); // Convierte de grados a radianes.
+    $dlon = deg2rad($lon2 - $lon1); // Convierte de grados a radianes.
+    $a = sin($dlat / 2) * sin($dlat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dlon / 2) * sin($dlon / 2);
     $b = 2 * atan2(sqrt($a), sqrt(1 - $a));
-    $c = round($radius * $b, 2); // Distancia en km con dos decimales de precisión.
+    $c = round(6371 * $b, 2); // Distancia en km con dos decimales de precisión.
     return $c;
 };
 
-function ($deg) {
-    return deg2rad($deg * (pi() / 180));
-};
-
-echo (latLonDist(-17.645820, -63.336103, -17.644973, -63.141274));
+echo (latlondist(-17.645820, -63.336103, -17.644973, -63.141274));
