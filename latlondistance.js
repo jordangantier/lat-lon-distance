f6186ab928218de5536af457c9dcf6aa67dbea3f
jordangantier <47302177+jordangantier@users.noreply.github.com>
2022-05-29 14:33:43 -0400

    Add files via upload

---
+++ latlondistance.js
@@ -0,0 +1,15 @@
+function getLatLonDist(lat1, lon1, lat2, lon2) {
+    var radius = 6371; // Radio de la tierra en km.
+    var dLat = deg2rad(lat2 - lat1); // Convierte de grados a radianes.
+    var dLon = deg2rad(lon2 - lon1); // Convierte de grados a radianes.
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var c = (radius * b).toFixed(2); // Distancia en km con dos decimales de precisión.
+    return c;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+}
+
+console.log(getLatLonDist(-17.645820, -63.336103, -17.940226, -62.999360));
